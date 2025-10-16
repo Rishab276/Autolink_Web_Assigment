43d4e12fbb678fb5fcc2431549dd12797c1d0ec4
Rishab276 <raghoonundunrishab276@gmail.com>
2025-10-16 21:49:15 +0400

    i have start backend on app vehicles , i have added model.py ,created 2 class vehicles,vehiclesimages,i have created superuser,some tables

--- Autolink/settings.py
+++ Autolink/settings.py
@@ -128,6 +128,16 @@ STATICFILES_DIRS = [
     BASE_DIR / "static", 
 ]
 
+import os
+
+# Existing settings...
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- Vehicles/models.py
+++ Vehicles/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    # Basic Info
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField()
+
+    # Specifications
+    transmission = models.CharField(max_length=50, choices=[
+        ('Automatic', 'Automatic'),
+        ('Manual', 'Manual'),
+        ('CVT', 'CVT'),
+        ('Other', 'Other'),
+    ])
+    fuel_type = models.CharField(max_length=50, choices=[
+        ('Petrol', 'Petrol'),
+        ('Diesel', 'Diesel'),
+        ('Hybrid', 'Hybrid'),
+        ('Electric', 'Electric'),
+        ('Other', 'Other'),
+    ])
+    type_of_vehicle = models.CharField(max_length=50, choices=[
+        ('Car', 'Car'),
+        ('Truck', 'Truck'),
+        ('Van', 'Van'),
+        ('SUV', 'SUV'),
+        ('Motorbike', 'Motorbike'),
+        ('Bus', 'Bus'),
+    ])
+
+    # Other Details
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    gps_coor = models.CharField(max_length=100, blank=True, null=True)
+    is_rental = models.BooleanField(default=False)
+    desc = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year})"
+    
+
+
+class VehicleImage(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='vehicle_images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.vehicle.make} {self.vehicle.model}"
+    
+       
+
+    
+
+
