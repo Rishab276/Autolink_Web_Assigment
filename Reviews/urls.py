18d8a786df7bcc3e922ef66378dae453a63a2cf2
Humaa19 <humaa.bukreedan@gmail.com>
2025-10-27 19:40:56 +0400

    i am pushing(review,report and recommendations)

--- Reviews/urls.py
+++ Reviews/urls.py
@@ -1,22 +1,13 @@
+#byhumaa
 from django.urls import path
 from . import views
 
+app_name = 'Reviews'
+
 urlpatterns = [
+    path('', views.review_page, name='review_page'),
+    path('report/', views.report_page, name='report_page'),
+    path('submit/', views.submit_review, name='submit_review'),
+    path('report/submit/', views.submit_report, name='submit_report'),
     path('recommendations/', views.recommendations_view, name='recommendations'),
-    path('report/', views.report_view, name='report'),
-    path('review/', views.review_view, name='review'),
 ]
-
-
-
-
-
-
-# urlpatterns = [
-#     path('', views.recommendations, name='recommendations'),
-# ]
-
-# urlpatterns = [
-#     path('', views.report, name='report'),
-# ]
-
