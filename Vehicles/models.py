ad51428974a87a150efb9b5b28f468fd8bb56bdc
vag27-creator <vbhewa77@gmail.com>
2025-11-22 11:46:35 +0400

    i am commiting my changes on based on my apps(users)

--- Vehicles/models.py
+++ Vehicles/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Vehicle(models.Model):
+class Vehicle(models.Model):   # carried out by Raghoonundun Rishab 2412024 and  Bhewa Vigneshwar 2411725(uploadvehicle)
+    
     uploader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploaded_vehicles', default=1)
-    # Basic Info
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
     mileage = models.PositiveIntegerField()
 
-    # Specifications
     transmission = models.CharField(max_length=50, choices=[
         ('Automatic', 'Automatic'),
         ('Manual', 'Manual'),
@@ -37,9 +36,7 @@ class Vehicle(models.Model):
     ],
         default='Car'
     )
-
-
-    # Other Details
+    
     price = models.DecimalField(max_digits=10, decimal_places=0)
     gps_coor = models.CharField(max_length=100, blank=True, null=True)
     is_rental = models.BooleanField(default=False)
