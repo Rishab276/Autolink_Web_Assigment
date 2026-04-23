a6e8283d6f3ed456d9b7c05cb69642dad2bc2936
salwan080604 <maighuns@gmail.com>
2026-04-23 17:53:07 +0400

    Removed IsAuthenticated from settings.py to avoid crash

--- Autolink/settings.py
+++ Autolink/settings.py
@@ -159,6 +159,5 @@ REST_FRAMEWORK = {
        ],
        'DEFAULT_PERMISSION_CLASSES': [
            'rest_framework.permissions.AllowAny',
-           'rest_framework.permissions.IsAuthenticated',
        ],
    }
